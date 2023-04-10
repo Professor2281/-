a0421fb9e53bd9f84a09b1737d248777e1e6370e
Влад Безедика <sttasea@mail.ru>
2023-04-10 15:58:54 +0300

    Add project files.

---
+++ Стих/Стих.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Home task #2.2\n\n";
+	cout << "Я узнал, что у меня" << "\n";
+	cout << "\tEсть огромная семья" << "\n";
+	cout << "\t\tИ тропинка и лесок" << "\n";
+	cout << "\t\t\tВ поле каждый колосок" << "\n";
+	cout << "\t\t\t\tРечка, небо голубое —" << "\n";
+	cout << "\t\t\t\t\t Это все мое родное" << "\n";
+	cout << "\t\t\t\t\t\tЭто Родина моя," << "\n";
+	cout << "\t\t\t\t\t\t\tВсех люблю на свете я!";
+	return 0;
+}
