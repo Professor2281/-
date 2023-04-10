75c10a45a15e7cee1cc91c816086be3f7e090a9c
Влад Безедика <sttasea@mail.ru>
2023-04-10 16:35:34 +0300

    dz doon

---
+++ Разноцвтный стих/Разноцвтный стих.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Home task #2.2\n\n";
+	cout << "Every" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\t\tWants" << "\n";
+	cout << "\t\t\tTo know" << "\n";
+	cout << "\t\t\t\t Where" << "\n";
+	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+	cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+	return 0;
+}
